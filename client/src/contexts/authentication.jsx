5d7fc5d3b8e7ac67edd401c0c2d46a135115f0b0
Shin <sirapop1998@gmail.com>
2025-09-10 01:25:24 +0700

    feat: add JWT authentication middleware and update client with dotenv integration

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import { useState, useContext, createContext } from "react";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const AuthContext = React.createContext();
+
+const AuthContext = createContext();
+
 
 function AuthProvider(props) {
   const [state, setState] = useState({
@@ -8,13 +13,28 @@ function AuthProvider(props) {
     error: null,
     user: null,
   });
+  const navigate = useNavigate();
 
-  const login = () => {
+  const login = async (data) => {
     // 🐨 Todo: Exercise #4
     //  ให้เขียน Logic ของ Function `login` ตรงนี้
     //  Function `login` ทำหน้าที่สร้าง Request ไปที่ API POST /login
     //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
-  };
+    try {
+      const result = await axios.post("http://localhost:4000/auth/login", data);
+      const token = result.data.token;
+      localStorage.setItem("token", token);
+      const userDataFromToken = jwtDecode(token);
+      setState({
+        ...state,
+        user: userDataFromToken,
+      });
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
 
   const register = () => {
     // 🐨 Todo: Exercise #2
@@ -24,9 +44,17 @@ function AuthProvider(props) {
   };
 
   const logout = () => {
-    // 🐨 Todo: Exercise #7
-    //  ให้เขียน Logic ของ Function `logout` ตรงนี้
-    //  Function logout ทำหน้าที่ในการลบ JWT Token ออกจาก Local Storage
+    // ลบ JWT Token ออกจาก Local Storage
+    localStorage.removeItem("token");
+    
+    // Update state ให้ user เป็น null
+    setState(prev => ({
+      ...prev,
+      user: null
+    }));
+    
+    // Navigate ไปที่หน้า Login
+    navigate("/login");
   };
 
   const isAuthenticated = Boolean(localStorage.getItem("token"));
@@ -41,6 +69,6 @@ function AuthProvider(props) {
 }
 
 // this is a hook that consume AuthContext
-const useAuth = () => React.useContext(AuthContext);
+const useAuth = () => useContext(AuthContext);
 
 export { AuthProvider, useAuth };
--- client/src/utils/jwtInterceptor.js
+++ client/src/utils/jwtInterceptor.js
@@ -2,10 +2,16 @@ import axios from "axios";
 
 function jwtInterceptor() {
   axios.interceptors.request.use((req) => {
-    // 🐨 Todo: Exercise #6
-    //  ให้เขียน Logic ในการแนบ Token เข้าไปใน Header ของ Request
-    // เมื่อมีการส่ง Request จาก Client ไปหา Server
-    // ภายใน Callback Function axios.interceptors.request.use
+    // แนบ Token เข้าไปใน Header ของ Request
+
+    const hasToken = Boolean(window.localStorage.getItem("token"));
+    
+    if (hasToken) {
+      req.headers = {
+        ...req.headers,
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+      };
+    }
 
     return req;
   });
@@ -15,10 +21,16 @@ function jwtInterceptor() {
       return req;
     },
     (error) => {
-      // 🐨 Todo: Exercise #6
-      //  ให้เขียน Logic ในการรองรับเมื่อ Server ได้ Response กลับมาเป็น Error
-      // โดยการ Redirect ผู้ใช้งานไปที่หน้า Login และลบ Token ออกจาก Local Storage
-      // ภายใน Error Callback Function ของ axios.interceptors.response.use
+      // รองรับเมื่อ Server Response Error (401 Unauthorized)
+      if (error.response.status === 401 &&
+        error.response.statusText === "Unauthorized"
+      ) {
+        // ลบ Token ออกจาก Local Storage
+        window.localStorage.removeItem("token");
+        
+        // Redirect ไปที่หน้า Login
+        window.location.replace = "/login";
+      }
 
       return Promise.reject(error);
     }
